353ad5180db55cb680445197b0c1e8c0186d4872
Kathy Nguyen <kathyn262@gmail.com>
2021-02-22 15:52:05 -0800

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Kathy Nguyen on 2/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

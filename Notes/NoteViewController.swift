d56c28d9724048caed9d9f7169eeb1182aad9414
Kathy Nguyen <kathyn262@gmail.com>
2021-02-23 15:28:52 -0800

    set up note detail screen

--- Notes/NoteViewController.swift
+++ Notes/NoteViewController.swift
@@ -8,22 +8,21 @@
 import UIKit
 
 class NoteViewController: UIViewController {
-
+    @IBOutlet var titleLabel : UILabel!
+    @IBOutlet var noteLabel: UITextView!
+    
+    // need to give these default values or will get "no initializers" error
+    public var noteTitle: String = ""
+    public var note: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        titleLabel.text = noteTitle
+        noteLabel.text = note
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }

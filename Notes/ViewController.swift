1c86f11f1f489595506ae6e422d91896935e9e69
Kathy Nguyen <kathyn262@gmail.com>
2021-02-22 16:45:13 -0800

    set up table

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var table: UITableView!
+    @IBOutlet var label: UILabel!
+    
+    var models: [(title: String, note: String)] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.delegate = self
+        table.dataSource = self
+        title = "Notes"
     }
 
-
+    @IBAction func didTapNewNote() {
+        
+    }
+    
+    // how many rows to display?
+    // crosscheck with other one
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row].title
+        cell.detailTextLabel?.text = models[indexPath.row].note
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // show Note controller
+        guard let vc = storyboard?.instantiateViewController(identifier: "note") as? NoteViewController else {
+            return
+        }
+        vc.title = "Note"
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
